51e1f8b18d475a7e7b3ee9266d74038903a0bd8e
rulasg <rulasg@github.com>
2025-06-09 15:09:16 +0200

    Refactor Program class and enhance tests to include argument handling

--- console1.cmd/Program.cs
+++ console1.cmd/Program.cs
@@ -1,12 +1,13 @@
-﻿
+﻿using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("console1.test")]
 namespace console1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Hello, World! {string.Join(" ", args)}");
         }
     }
 }
--- console1.test/Test1.cs
+++ console1.test/Test1.cs
@@ -1,12 +1,48 @@
-﻿namespace console1.test;
+﻿using System.IO;
+using System.Text;
+using Moq;
+
+
+namespace console1.test;
 
 [TestClass]
-public sealed class Test1
+public sealed class ProgramTests
 {
+
+    StringBuilder _consoleOutput;
+    Mock<TextReader> _consoleInput;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _consoleOutput = new StringBuilder();
+        _consoleInput = new Mock<TextReader>();
+        Console.SetOut(new StringWriter(_consoleOutput));
+        Console.SetIn(_consoleInput.Object);
+    }
+
+    private string[] RunAndGetConsoleOutput(string[] args)
+    {
+        Program.Main(args);
+        return _consoleOutput.ToString().Split(Environment.NewLine);
+    }
+
+    private MockSequence SetupUserResponses(params string[] userResponses)
+    {
+        var sequence = new MockSequence();
+        foreach (var response in userResponses)
+            _consoleInput.InSequence(sequence).Setup(x => x.ReadLine()).Returns(response);
+
+        return sequence;
+    }
+
     [TestMethod]
     public void TestMethod1()
     {
+        SetupUserResponses("input1", "input2");
+        var output = RunAndGetConsoleOutput(new[] { "arg1", "arg2" });
 
+        Assert.AreEqual("Hello, World! arg1 arg2", output[0]);
 
     }
 }
